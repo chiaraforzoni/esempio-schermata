bc6c3f00d073b6a6b648b57021698f54f5fd5cc3
chiaraforzoni <chiara.forzoni@mail.polimi.it>
2017-02-23 16:05:37 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+//createCanvas(500,500);
+ // background (20,20,20);
+}
+
+function draw() {
+  function doIt(){
+ parent.NOME_DEL_FRAME.location.href=durl[sctr];
+ }
+
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
